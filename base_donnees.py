f588c9f7074ada3e791e9cc1898b3778dcd899f3
TousignantC <39924728+TousignantC@users.noreply.github.com>
2019-02-12 14:22:41 -0500

    depart BC

--- base_donnees.py
+++ base_donnees.py
@@ -1,14 +1,47 @@
-import cx_Oracle
-#Connect as user "hr" with password "welcome" to the "raclepdb" service running on this computer.
-connection = cx_Oracle.connect("GARI", "Inrs2019", "192.168.0.233")
-#connection = cx_Oracle.connect('sales.us.oracle.com')
 
-cursor = connection.cursor()
-cursor.execute("""
-    SELECT first_name, last_name
-    FROM employees
-    WHERE department_id = :did AND employee_id > :eid""",
-    did = 50,
-    eid = 190)
-for fname, lname in cursor:
-    print("Values:", fname, lname)
+import cx_Oracle
+
+dsn_tns = cx_Oracle.makedsn('192.168.0.233', 1521, service_name='EMGSPROD2012')
+conn = cx_Oracle.connect(user=r'GARI', password='Inrs2019', dsn=dsn_tns)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# cursor.execute("""
+#     SELECT first_name, last_name
+#     FROM employees
+#     WHERE department_id = :did AND employee_id > :eid""",
+#     did = 50,
+#     eid = 190)
+# for fname, lname in cursor:
+#     print("Values:", fname, lname)
